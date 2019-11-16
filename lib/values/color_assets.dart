fb153fbe8aaaced5b734fb5a353a1c58ea33faaf
Jeremiah Jacob <jaykon4me@gmail.com>
2019-11-16 19:39:48 +0100

    create color asset

--- lib/values/color_assets.dart
+++ lib/values/color_assets.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class ColorAssets {
   static final Color primarySwatchColor = Color(0xFF827FF0);
   static final Color accentColor = Color(0xFF5996B4);
-  static final Color primaryTextColor = Colors.grey[900];
+  static final Color primaryTextColor = Colors.grey[600];
 }
